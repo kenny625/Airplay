9669f9c4dfef1a961520f47d0c6f8cd28745ee74
WeiChen <kenny625@gmail.com>
2013-02-28 15:01:35 +0800

    modified

--- AirPlayTest/HWViewController.m
+++ AirPlayTest/HWViewController.m
@@ -7,10 +7,13 @@
 //
 
 #import "HWViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
+#import "MPAudioDeviceController.h"
 
 @interface HWViewController ()
 
 @end
+
 
 @implementation HWViewController
 
@@ -18,12 +21,48 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self pickAMirroredRoute];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)pickAMirroredRoute {
+    MPAudioDeviceController *audoDeviceController = [[MPAudioDeviceController alloc] init];
+    audoDeviceController.routeDiscoveryEnabled = YES;
+    
+    [audoDeviceController determinePickableRoutesWithCompletionHandler:^(NSInteger value) {
+        NSMutableArray *routes = [NSMutableArray array];
+        [audoDeviceController clearCachedRoutes];
+        
+        NSUInteger index = 0;
+        while (true) {
+            NSDictionary *route = [audoDeviceController routeDescriptionAtIndex:index];
+            if (route) {
+                [routes addObject:route];
+                index++;
+            } else {
+//                NSLog(@"%@", routes);
+                break;
+            }
+        }
+        
+        [routes enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+            if ([[obj objectForKey:@"RouteSupportsAirPlayScreen"] boolValue]) {
+                NSDictionary *info = [obj objectForKey:@"AirPlayPortExtendedInfo"];
+                NSString *deviceID = [info objectForKey:@"deviceID"];
+                if ([[info objectForKey:@"uid"] isEqualToString:[NSString stringWithFormat:@"%@-screen", deviceID]]) {
+                    [audoDeviceController pickRouteAtIndex:idx];
+                    //有多台apple TV時，把這個idx存到一個地方，之後就可以根據idx選要用哪一台mirror
+                }
+            }
+            NSLog(@"%@  ",obj);
+        }];
+    }];
+    
 }
 
 @end
